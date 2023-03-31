d271f15f3f3c1594e9117943f24c43e2864fcd13
Major2000 <majormadobe@gmail.com>
2023-03-31 22:26:51 +0300

    Service Context and Index

--- chatdocs.py
+++ chatdocs.py
@@ -13,3 +13,7 @@ os.environ["OPENAI_API_KEY"] = "sk-WfVpmVdMbRa5jzGrk6d3T3BlbkFJW7YFjX1qselLYfRdc
 documents = SimpleDirectoryReader("documents").load_data()
 
 llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+
